466036d668296653ceedc50d43f6687f78f670a7
bobo23 <bosobieski@gmail.com>
2022-06-17 09:46:32 +0200

    working on exeption tests

--- test/exceptions.mjs
+++ test/exceptions.mjs
@@ -1,6 +1,7 @@
 import Be8 from './bundle.mjs';
 
-const be8 = new Be8('1');
+const be8Sender = new Be8('1');
+const be8Receiver = new Be8('2');
 
 QUnit.module('Exceptions');
 
@@ -12,8 +13,26 @@ QUnit.test('Throw at constructor due to no id', async function (assert) {
     assert.throws(fn, /no acc id passed to constructor/, 'Throw at constructor due to no id');
 });
 
-QUnit.test('Throw at encryptTextSimple due to no id', async function (assert) {
+QUnit.test('Throw at encryptTextSimple due to no sender key', async function (assert) {
+    const text = 'Hallo Welt';
+    const [publicKeyRECEIVER] = await be8Receiver.generatePrivAndPubKey();
+    
+    be8Receiver.addPublicKey('2', publicKeyRECEIVER);
 
+    const prom = be8Receiver.encryptTextSimple('1', '2', text);
+    
+    return assert.rejects(prom, /Missing private key for 1 at encryptTextSimple/, 'Throw at encryptTextSimple due to no sender key');
+});
+
+QUnit.test('Throw at encryptTextSimple due to no receiver key', async function (assert) {
+    const text = 'Hallo Welt';
+    const [publicKeySENDER] = await be8Sender.generatePrivAndPubKey();
+    
+    be8Sender.addPublicKey('1', publicKeySENDER);
+
+    const prom = be8Sender.encryptTextSimple('1', '2', text);
+    
+    return assert.rejects(prom, /Missing public key for 2 at encryptTextSimple/, 'Throw at encryptTextSimple due to no sender key');
 });
 
 QUnit.test('Throw at getDerivedKey due to missing public key' , async function (assert) {
