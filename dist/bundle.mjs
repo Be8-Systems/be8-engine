5b4e3c519cd40ebad3d1e6c1e62db706d7792625
bobo23 <bosobieski@gmail.com>
2022-07-11 16:15:00 +0200

    added addGroupKeys

--- dist/bundle.mjs
+++ dist/bundle.mjs
@@ -170,6 +170,7 @@ class Be8 {
                 this.#groupKeys.set(`${groupID}:${version}`, groupKey)
             );
             const proms = groupKeys.map(function ({ version, groupKey }) {
+                console.log(groupID, version, ...groupKey);
                 groupKeysStore.put({ groupID, version, ...groupKey });
                 groupKeysStore.onsuccess = () =>
                     console.log(`added group key for ${groupID}`);
